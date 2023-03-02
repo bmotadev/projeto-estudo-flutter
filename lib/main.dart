9e3862ff7369fccd11c3a72f59794097d53e9170
Bruno Mota <bmotadev@gmail.com>
2023-03-02 20:53:18 -0300

    :lipstick: feat: alterado a interface do app - utilizando o Material e MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,21 @@ void main(List<String> args) {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Bruno Mota',
-        textDirection: TextDirection.rtl,
-      ),
+    return MaterialApp(
+      //instanciado o HomeWidget. Assim tendo a criação e componentização de widgets de forma separada.
+      home: HomePageWidget(),
     ); //estamos passando a instancia de outra classe diretamente para o retorno dela.
+  }
+}
+
+class HomePageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text('Bruno Mota'),
+      ),
+    );
+    // ao utilizar o StalessWidget, ele pede para implementar o metodo build. Basta retornar qualquer widget dentro dele.
   }
 }
