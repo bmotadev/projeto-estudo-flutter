ce7306ec081291cc083ae5a2bd0fda256db45136
Bruno Mota <bmotadev@gmail.com>
2023-03-02 21:24:54 -0300

    :lipstick: feat: utilizando Scaffold - appBar, Body, Icon, FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,20 @@ class HomePageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        // barra no topo da tela
+        title: const Text('data'),
+      ),
+      body: const Center(
         child: Text('Bruno Mota'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        // quando clicar nesse botão, vai chamar a função anonima abaixo. Uma função anonima pode ser passada como propriedade.
+        onPressed: () {
+          print('Clicou');
+        },
       ),
     );
     // ao utilizar o StalessWidget, ele pede para implementar o metodo build. Basta retornar qualquer widget dentro dele.
