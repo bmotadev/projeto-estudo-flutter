b7b07dc368692723b53f117d369430f925c5dd7a
Bruno Mota <bmotadev@gmail.com>
2023-03-11 12:31:04 -0300

    :recycle: refactor: widget HomePage - separado o widget em outro arquivo

--- lib/ myapp.dart
+++ lib/ myapp.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePageWidget extends StatelessWidget {
+  const HomePageWidget(
+      {super.key}); // key é basicamente um identificador na arvore do widget. É opcional.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // barra no topo da tela
+        title: const Text('data'),
+      ),
+      body: const Center(
+        child: Text('Bruno Mota'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        // quando clicar nesse botão, vai chamar a função anonima abaixo. Uma função anonima pode ser passada como propriedade.
+        onPressed: () {
+          print('Clicou');
+        },
+      ),
+    );
+    // ao utilizar o StalessWidget, ele pede para implementar o metodo build. Basta retornar qualquer widget dentro dele.
+  }
+}
